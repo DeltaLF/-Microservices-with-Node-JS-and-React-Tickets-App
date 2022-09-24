65dc0147af8b0e5bab2725318fa9d0a5684bea70
DeltaLF <patricktsaikl@gmail.com>
2022-09-24 08:29:03 +0800

    add one auth route and ingress config to map port to url route

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,8 +4,14 @@ import {json} from 'body-parser';
 const app = express()
 app.use(json())
 
+app.get('/api/users/currentuser',(req,res)=>{
+    res.send('Hi currentUser')
+})
+
 app.listen(3000,()=>{
     console.log("auth server is running on port 3000")
 })
+
+
 
 
