6dbb2d05a1100641e178492ef9f5bce4a4bc5b64
DeltaLF <patricktsaikl@gmail.com>
2022-09-25 13:41:35 +0800

    implement user model, auth signup

--- auth/src/middleware/error-handler.ts
+++ auth/src/middleware/error-handler.ts
@@ -7,6 +7,7 @@ export const errorHandler = (err:Error, req:Request, res:Response, next:NextFunc
         return res.status(err.statusCode).send({ errors: err.serializeErrors()})
     }
 
+    console.error("errorHandler catching ", err)
     // general error
     res.status(400).send({
         erros:[{ message:'something went wrong' }]
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,8 @@
 import express, {Request, Response} from "express"
 import {body, validationResult} from "express-validator"
+import { User } from "./../models/user"
 import { RequestValidationError } from "../errors/request-validation-error"
-import { DatabaseConnectionError } from "../errors/database-connection-error"
+import { BadRequestError } from "../errors/bad-request-error"
 
 const router = express.Router()
 
@@ -15,7 +16,8 @@ router.post('/api/users/signup', [
     .trim()
     .isLength({min:4,max:20})
     .withMessage('Password must be between 4 and 20 characters')
-],(req:Request,res:Response)=>{
+],
+async (req:Request,res:Response) => {
     const errors = validationResult(req)
     if(!errors.isEmpty()){
         // catched by erorr handling middleware
@@ -23,10 +25,15 @@ router.post('/api/users/signup', [
         throw new RequestValidationError(errors.array())
     }
    const {email, password} = req.body
-   console.log("creating a user...")
-   throw new DatabaseConnectionError()
-   res.send({})
+   const existingUser = await User.findOne({email})
+   
+   if(existingUser){
+    throw new BadRequestError('Email in use')
+   }
+   const user = User.build({email, password})
+   await user.save()
+   res.status(201).send(user)
 
-})
+});
 
 export {router as signupRouter}
