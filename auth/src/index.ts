00d39b88c76bb19b4303e3a5e7dd40619ccd6022
DeltaLF <patricktsaikl@gmail.com>
2022-09-25 10:54:02 +0800

    setup mongo pod and connection between auth and mongo

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import 'express-async-errors'
 import {json} from 'body-parser';
+import mongoose from 'mongoose'
+
 import {currentUserRouter} from "./routes/current-user"
 import { signinRouter } from './routes/sigin';
 import { signoutRouter } from './routes/signout';
@@ -25,10 +27,21 @@ app.all('*',async (req,res,next)=>{
 
 app.use(errorHandler)
 
-app.listen(3000,()=>{
-    console.log("auth server is running on port 3000")
-})
+const start = async()=>{
+    // the connection is actually accros pod 
+    try{
+        await mongoose.connect('mongodb://auth-mongo-srv:27017')
+        console.log("Connected to Mongodb")
+    } catch (err){
+        console.error(err)
+    }
+    app.listen(3000,()=>{
+        console.log("auth server is running on port 3000!")
+    })   
+   
+}
 
+start();
 
 
 
