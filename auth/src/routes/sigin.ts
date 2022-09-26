e56e4ae577d5f671a08e70782cb4451c8da9b098
DeltaLF <patricktsaikl@gmail.com>
2022-09-26 20:27:35 +0800

    implement signin, validate request middleware

--- auth/src/routes/sigin.ts
+++ auth/src/routes/sigin.ts
@@ -1,10 +1,50 @@
-import express from "express"
+import express,{Request, Response} from "express"
+import {body } from 'express-validator'
+import jwt from "jsonwebtoken"
+
+import {Password} from "../services/password"
+import {User} from "../models/user"
+import { validateRequest } from "../middleware/validate-request";
+import { BadRequestError } from "../errors/bad-request-error";
 
 const router = express.Router()
 
 
-router.post('/api/users/signin',(req,res)=>{
-    res.send("hi!")
+router.post('/api/users/signin',[
+    body('email')
+    .isEmail()
+    .withMessage('Email must be valid'),
+    body('password')
+    .trim()
+    .notEmpty()
+    .withMessage('You must supply a password')
+],
+validateRequest,
+  async (req:Request,res:Response) => {
+    const {email, password } = req.body;
+    const existingUser = await User.findOne({email});
+    if(!existingUser){
+        throw new BadRequestError('Invalid credentials');
+        //  no need to offter to much error message
+        
+    }
+    const passwordsMatch = await Password.compare(existingUser.password, password);
+
+    if(!passwordsMatch){
+        throw new BadRequestError('Invlaid Credentials');
+    }
+
+     // generat JWT
+   const userJwt = jwt.sign({
+    id: existingUser.id,
+    email: existingUser.email
+   },process.env.JWT_KEY!)
+
+   req.session = {
+    jwt: userJwt
+   }
+
+   res.status(200).send(existingUser)
 })
 
 export {router as signinRouter}
