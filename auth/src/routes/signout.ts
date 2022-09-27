1f9a8b53557c6eea38d29c6e12fd3a965e539053
DeltaLF <patricktsaikl@gmail.com>
2022-09-27 08:42:14 +0800

    implement middlewares to append and check auth

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -4,7 +4,9 @@ const router = express.Router()
 
 
 router.post('/api/users/signout',(req,res)=>{
-    res.send("hi!")
+    // dump the cookie in FE
+    req.session = null;
+    res.send({})
 })
 
 export {router as signoutRouter}
