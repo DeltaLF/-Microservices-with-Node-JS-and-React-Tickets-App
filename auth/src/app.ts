01e13be31cff7f59fa7bd3f0ef7663c0efe7c24a
DeltaLF <patricktsaikl@gmail.com>
2022-09-28 20:37:15 +0800

    test for singup, signin, signout, currentuser

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -19,7 +19,7 @@ app.use(json())
 app.use(
     cookieSession({
         signed: false,
-        secure: true // cookie only be used while connecting with https
+        secure: process.env.NODE_ENV !== 'test' // cookie only be used while connecting with https
     })
 )
 
