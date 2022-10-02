95cbadeceda13edc37d846dba7d2baf5e7413f30
DeltaLF <patricktsaikl@gmail.com>
2022-10-02 11:05:30 +0800

    refactor getInitialProps in nextJS client server(proxy server)

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import buildClient from "../api/build-client";
 
 const HomePage = ({ currentUser }) => {
   console.log(currentUser);
@@ -6,50 +6,13 @@ const HomePage = ({ currentUser }) => {
   return <div>Home Page </div>;
 };
 
-HomePage.getInitialProps = async ({ req }) => {
-  // req is the initila request to the nextjs
+HomePage.getInitialProps = async (context) => {
+  // nextJS client servre is a proxy server
+  // what the browser has is appended on the nextJS client server
+  const axiosInstance = buildClient(context);
+  const { data } = await axiosInstance.post("/api/users/currentuser");
 
-  // make request in server
-  // const response = await axios.get("/api/users/currentuser");
-  // cause some errors
-  // no domain specified axios will assuem it is current domain
-  // and waht exactly the 'current domain' is?
-  // it's the client pod docker container
-
-  if (typeof window === "undefined") {
-    // we are on the server nextjs
-    // reqeusts should be made to
-    // http://SERVICENAME.NAMESPACE.svc.cluster.local
-
-    const response = await axios
-      .post(
-        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
-        {},
-        {
-          headers: req.headers,
-          // headers: {
-          //   host: "ticketing.dev", // for specifying host in ingress
-          // },
-        }
-      )
-      .catch((err) => {
-        console.log("error in nextjs server getInitialProps", err);
-        return {};
-      });
-
-    console.log("succes in nextjs server getInitialProps", response.data);
-    return response.data || {};
-  } else {
-    // we are on the browser
-    // requests can be made with a base url of ''
-    const response = await axios.post("/api/users/currentuser");
-    // {currentuser:{}}
-    console.log(
-      "window not undefined, it's a redirect getInitialProps",
-      response.data
-    );
-    return response.data;
-  }
+  return data;
 };
 
 export default HomePage;
