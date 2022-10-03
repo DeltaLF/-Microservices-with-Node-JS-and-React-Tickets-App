de53140159caa942fcb673c63e9899e95861e38f
DeltaLF <patricktsaikl@gmail.com>
2022-10-03 19:54:30 +0800

    tickets service infra setup

---
+++ tickets/src/app.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import 'express-async-errors'
+import {json} from 'body-parser';
+import { errorHandler, NotFoundError } from '@tickets_dl/common';
+import cookieSession from "cookie-session"
+
+
+
+
+const app = express()
+app.set('trust proxy',true) // to pass ingress proxy
+app.use(json())
+app.use(
+    cookieSession({
+        signed: false,
+        secure: process.env.NODE_ENV !== 'test' // cookie only be used while connecting with https
+    })
+)
+
+
+
+app.all('*',async (req,res,next)=>{
+   throw new NotFoundError()
+})
+
+app.use(errorHandler)
+
+export { app };
--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -2,8 +2,9 @@ import mongoose from 'mongoose'
 import {app} from "./app"
 
 const start = async()=>{
+
     if(!process.env.JWT_KEY){
-        throw new Error('JWT_KEY must be defined')
+        throw new Error('JWT_KEY must be defined');
     }
     if(!process.env.MONGO_URI){
         throw new Error('MONGO_URI must be defined');
@@ -15,6 +16,7 @@ const start = async()=>{
     } catch (err){
         console.error(err)
     }
+
     app.listen(3000,()=>{
         console.log("auth server is running on port 3000!")
     })   
