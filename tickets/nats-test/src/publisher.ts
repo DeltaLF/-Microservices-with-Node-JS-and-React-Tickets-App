bf4421a48647cc237d471ab0687e2658346fdd34
DeltaLF <patricktsaikl@gmail.com>
2022-10-10 19:19:03 +0800

    basic publisher, listener setup

--- tickets/nats-test/src/publisher.ts
+++ tickets/nats-test/src/publisher.ts
@@ -1,10 +1,25 @@
 import nats from "node-nats-streaming";
 
-// also can be called client, which is the instance of nats
+// console.clear();
+
+//  client                  clusterID, clientID
 const stan = nats.connect('ticketing', 'abc',{
     url: 'http://localhost:4222'
 });
 
 stan.on('connect', ()=>{
-    console.log('Publisger connected to NATS')
+    console.log('Publisher connected to NATS!')
+
+    // we can only share stream, raw data
+    const data = JSON.stringify({ 
+        id:'123',
+        title:'concert',
+        price:100
+    });
+
+    // publish(subjectname, data, cb)
+    stan.publish('ticket:created',data,()=>{
+        console.log("Event published!")   
+    })
+
 });
