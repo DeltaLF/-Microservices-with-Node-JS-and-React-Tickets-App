7a6e210fcec9709eceec4fd281e77dd87b653a21
DeltaLF <patricktsaikl@gmail.com>
2022-10-11 20:01:45 +0800

    add manual ack and graceful shutdown

--- tickets/nats-test/src/listener.ts
+++ tickets/nats-test/src/listener.ts
@@ -11,14 +11,26 @@ const stan = nats.connect('ticketing', randomBytes(4).toString('hex'),{
 stan.on('connect',()=>{
     console.log("Listener connected to NATS!");
 
-    const subscription = stan.subscribe('ticket:created'); 
+    stan.on('close', ()=>{
+        console.log("NATS listener connection closed!")
+        process.exit();
+    });
+
+    const options = stan.subscriptionOptions()
+      .setManualAckMode(true);  // anknowledge mode (queue only confrim the event is correctly processed after receive ack)
+    // queue distributes event to only one of listener to prevent duplicated handling 
+    const subscription = stan.subscribe('ticket:created', 'orders-service-queue-group', options); 
 
     subscription.on('message', (msg:Message)=>{ 
         const data = msg.getData();
         if( typeof data === 'string'){
             console.log(`Received event #${msg.getSequence()}, with data: ${data}`)
         }
+        msg.ack();
     });
 });
 
 
+// close before intercepted or terminated
+process.on('SIGNINT', () => stan.close()); // intercept 
+process.on('SIGTERM',() => stan.close());  // terminate 
