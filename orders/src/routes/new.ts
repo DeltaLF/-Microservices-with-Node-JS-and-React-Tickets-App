af47d5fa2308665481fac0d0f0c149e829595ec9
DeltaLF <patricktsaikl@gmail.com>
2022-10-28 23:15:40 +0800

    orders service port orders route

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,15 @@
 import express,{Request, Response} from "express";
-
+import { requireAuth, validateRequest } from "@tickets_dl/common";
+import {body} from "express-validator";
 
 const router = express.Router();
 
-router.post('/api/orders',async (req:Request,res:Response)=>{
+router.post('/api/orders',requireAuth,[
+    body('ticketId')
+      .not()
+      .isEmpty()
+      .withMessage('TicketId must be provided')
+],async (req:Request,res:Response)=>{
     res.send({});
 })
 
