361fff2131ce19ab0aa043bc762813e0cc107ca9
DeltaLF <patricktsaikl@gmail.com>
2022-10-31 22:48:00 +0800

    implement delete order route, test

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,10 +1,29 @@
 import express,{Request, Response} from "express";
-
+import { NotAuthorizedError, NotFoundError, requireAuth } from "@tickets_dl/common";
+import { Order ,OrderStatus} from "../../models/order";
 
 const router = express.Router();
 
-router.delete('/api/orders/:orderId',async (req:Request,res:Response)=>{
-    res.send({});
+router.delete('/api/orders/:orderId',requireAuth ,async (req:Request,res:Response)=>{
+    // update the order status to cancel
+    const {orderId} = req.params;
+    const order = await Order.findById(orderId);
+
+    if(!order){
+        throw new NotFoundError();
+    }
+    if(order.userId !== req.currentUser.id){
+        throw new NotAuthorizedError();
+    }
+    // const order = await Order.findByIdAndUpdate(orderId,{status: OrderStatus.Cancelled});
+    order.status = OrderStatus.Cancelled;
+    await order.save();
+    
+    // todo
+    // publishing an event saying this was cancelled!
+
+
+    res.status(204).send(order);
 })
 
 export {router as deleteOrderRouter};
