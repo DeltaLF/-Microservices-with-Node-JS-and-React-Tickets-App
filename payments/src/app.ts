84c29ffcdf71a2e3a68a33661fc533337d45c3ab
Patrick <patricktsaikl@gmail.com>
2022-11-11 07:05:38 +0800

    basic setup for payments service

---
+++ payments/src/app.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import 'express-async-errors'
+import {json} from 'body-parser';
+import { errorHandler, NotFoundError, currentUser } from '@tickets_dl/common';
+import cookieSession from "cookie-session"
+
+
+
+const app = express()
+app.set('trust proxy',true) // to pass ingress proxy
+app.use(json())
+app.use(
+    cookieSession({
+        signed: false,
+        secure: process.env.NODE_ENV !== 'test' // cookie only be used while connecting with https
+    })
+)
+app.use(currentUser);
+
+app.all('*',async (req,res,next)=>{
+   throw new NotFoundError()
+})
+
+app.use(errorHandler)
+
+export { app };
