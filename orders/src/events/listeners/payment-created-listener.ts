267b89f0e32d91dc2b6098e2be73b813b73e59f5
Patrick <patricktsaikl@gmail.com>
2022-11-20 08:52:17 +0800

    add payment created listener in order service

---
+++ orders/src/events/listeners/payment-created-listener.ts
@@ -0,0 +1,19 @@
+import { Listener, OrderStatus, PaymentCreatedEvent, Subjects } from "@tickets_dl/common";
+import { Message } from "node-nats-streaming";
+import { Order } from "../../models/order";
+import { queueGroupName } from "./queue-group-name";
+
+export class PaymentCreatedListener extends Listener<PaymentCreatedEvent>{
+    subject: Subjects.PaymentCreated = Subjects.PaymentCreated;
+    queueGroupName = queueGroupName;
+    async onMessage(data: PaymentCreatedEvent['data'], msg:Message){
+        const order = await Order.findById(data.orderId);
+        if(!order){
+            throw new Error('Order not found!')
+        }
+        order.set({status: OrderStatus.Complete});
+        await order.save();
+        // should emit a order updated event to make version correct
+        msg.ack();
+    }
+}
