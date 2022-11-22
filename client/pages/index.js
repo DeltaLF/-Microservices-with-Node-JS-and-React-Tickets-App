9ad212d18f80a17417842c2ec803785ec6265be7
Patrick <patricktsaikl@gmail.com>
2022-11-22 08:37:02 +0800

    client service fetch tickets with getInitialProps, show tickets page

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,9 +1,31 @@
-const HomePage = ({ currentUser }) => {
-  return <h1>{currentUser ? "You are signed in" : "You are not signed in"}</h1>;
+const HomePage = ({ tickets }) => {
+  const ticketList = tickets.map((ticket) => {
+    return (
+      <tr key={ticket.id}>
+        <td>{ticket.title}</td>
+        <td>{ticket.price}</td>
+      </tr>
+    );
+  });
+  return (
+    <div>
+      <h1>tickets</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Titke</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>{ticketList}</tbody>
+      </table>
+    </div>
+  );
 };
 
-HomePage.getInitialProps = async (context, client, currentUser) => {
-  return {};
+HomePage.getInitialProps = async (context, axiosInstance, currentUser) => {
+  const { data } = await axiosInstance.get("/api/tickets");
+  return { tickets: data };
   /* currentUser will be fetched in APP component
   // nextJS client server is a proxy server
   // what the browser has is appended on the nextJS client server
